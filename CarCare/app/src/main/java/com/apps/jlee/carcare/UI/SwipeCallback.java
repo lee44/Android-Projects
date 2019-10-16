9aff4c334ce06fbb5816c192188721d8ff048faf
Lee44 <jlee7772@yahoo.com>
2019-10-16 15:52:42 -0700

    Fixed icon position on swipe. Need to edit them at home

--- CarCare/app/src/main/java/com/apps/jlee/carcare/UI/SwipeCallback.java
+++ CarCare/app/src/main/java/com/apps/jlee/carcare/UI/SwipeCallback.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.View;
 
 import com.apps.jlee.carcare.Adapters.GasAdapter;
@@ -26,7 +27,7 @@ public class SwipeCallback extends ItemTouchHelper.SimpleCallback
         //In super, we are disabling up and down swipes by passing 0 and enabling left and right swipes
         super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
         this.g = g;
-        delete_icon = ContextCompat.getDrawable(g.getContext(), R.drawable.trash);
+        delete_icon = ContextCompat.getDrawable(g.getContext(), R.drawable.trash_slide_left);
         edit_icon = ContextCompat.getDrawable(g.getContext(), R.drawable.edit);
         background = new ColorDrawable();
     }
@@ -57,17 +58,21 @@ public class SwipeCallback extends ItemTouchHelper.SimpleCallback
 
         View itemView = viewHolder.itemView;
         int backgroundCornerOffset = 20; //so background is behind the rounded corners of itemView
+        double scale = .6;
 
-        int iconMargin = (itemView.getHeight() - delete_icon.getIntrinsicHeight()) / 2;
-        int iconTop = itemView.getTop() + (itemView.getHeight() - delete_icon.getIntrinsicHeight()) / 2;
-        int iconBottom = iconTop + delete_icon.getIntrinsicHeight();
+        int icon_width = (int)(delete_icon.getIntrinsicWidth() * scale);
+        int icon_height = (int)(delete_icon.getIntrinsicHeight() * scale);
+
+        int iconTop = itemView.getTop() + icon_height;
+        int iconBottom = itemView.getTop() + itemView.getHeight() - icon_height;
+        int iconMargin = (int)(delete_icon.getIntrinsicHeight() * .2);
 
         if (dX > 0)
         { // Swiping to the right
             background.setColor(rightswipe_color);
-            int iconLeft = itemView.getLeft() + iconMargin + edit_icon.getIntrinsicWidth();
-            int iconRight = itemView.getLeft() + iconMargin;
-            edit_icon.setBounds(iconRight, iconTop, iconLeft, iconBottom);
+            int iconLeft = itemView.getLeft() + iconMargin;
+            int iconRight = itemView.getLeft() + icon_width + iconMargin;
+            edit_icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
             background.setBounds(itemView.getLeft(), itemView.getTop(), itemView.getLeft() + ((int) dX) + backgroundCornerOffset, itemView.getBottom());
             background.draw(c);
             edit_icon.draw(c);
@@ -75,7 +80,7 @@ public class SwipeCallback extends ItemTouchHelper.SimpleCallback
         else if (dX < 0)
         { // Swiping to the left
             background.setColor(leftswipe_color);
-            int iconLeft = itemView.getRight() - iconMargin - delete_icon.getIntrinsicWidth();
+            int iconLeft = itemView.getRight() - icon_width - iconMargin;
             int iconRight = itemView.getRight() - iconMargin;
             delete_icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
             background.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset, itemView.getTop(), itemView.getRight(), itemView.getBottom());
